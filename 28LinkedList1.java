ba019f8632681ec6e5fe224fe5a205f89a8f14b3
Shivam Mishra <126499350+shivammishra22@users.noreply.github.com>
2026-08-03 00:48:25 +0530

    Implement MyLinkedList1 with basic operations
    This class implements a linked list with methods to add nodes, print the list, search for a node, and get a value at a specific position.

---
+++ 28LinkedList1.java
@@ -0,0 +1,95 @@
+//Creating Linked List: Insertion at End
+
+class MyLinkedList1{
+	private Node head;
+	private Node tail;
+	private class Node{
+		int data;
+		Node next;
+		Node(int data){
+			this.data=data;
+		}
+	}
+	public void addNodeAtEnd(int data) {
+		Node newNode=new Node(data);
+		if(head==null) {
+			head=tail=newNode;
+		}else {
+			tail.next=newNode;
+			tail=newNode;
+		}
+	}
+	public void printList() {
+		if(head==null) {
+			System.out.println("Linked List is EMPTY!");
+			return;
+		}
+		System.out.println("Nodes of Linked list: ");
+		Node currentNode=head;
+		while(currentNode!=null) {
+			System.out.print(currentNode.data+" ");
+			currentNode=currentNode.next;
+		}
+		System.out.println();
+	}
+
+	public void printListInReverse() {
+		//your logic
+	}
+	public void searchNode(int v) {
+		if(head==null) {
+			System.out.println("Linked List is EMPTY!");
+			return;
+		}
+		boolean flag=false;
+		Node currentNode=head;
+		while(currentNode!=null) {
+			if(currentNode.data==v) {
+				flag=true;
+				break;
+			}
+			currentNode=currentNode.next;
+		}
+		if(flag) {
+			System.out.println(v+ " Found!");
+		}else {
+			System.out.println(v+ " NOT Found!");
+		}
+	}
+	public void getValueAt(int n) {
+		boolean flag=false;
+		Node currentNode=head;
+		while(currentNode!=null) {
+			if(n--==1) {
+				flag=true;
+				break;
+			}
+			currentNode=currentNode.next;
+		}
+		if(flag) {
+			System.out.println("Value = "+currentNode.data);
+		}else {
+			System.out.println("Number Out of Boundary!");
+		}
+	}
+}
+public class LinkedList1 {
+
+	public static void main(String[] args) {
+		MyLinkedList1 list=new MyLinkedList1();
+		list.printList();
+		list.addNodeAtEnd(4);
+		list.addNodeAtEnd(7);
+		list.addNodeAtEnd(8);
+		list.addNodeAtEnd(3);
+		list.addNodeAtEnd(9);
+		list.addNodeAtEnd(2);
+		list.printList();
+		list.searchNode(90);
+		list.searchNode(9);
+		list.getValueAt(9);
+		list.getValueAt(3);
+		
+	}
+
+}
