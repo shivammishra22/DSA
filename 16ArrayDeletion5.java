7583767df21d887e1e1f231a4d959bd1e29c026f
Shivam Mishra <126499350+shivammishra22@users.noreply.github.com>
2026-08-03 00:38:42 +0530

    Add array value deletion method in ArrayDeletion5
    Implemented array value deletion functionality.

---
+++ 16ArrayDeletion5.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayDeletion5 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+//		System.out.println("Enter Array Size:");
+//		int n=sc.nextInt();
+//		int []arr=new int[n];
+//		for(int i=0;i<arr.length;i++) {
+//			System.out.println("Enter Array Value:");
+//			arr[i]=sc.nextInt();
+//		}
+		
+		//or
+		
+		int []arr= {7,25,8,145,8,1,5,1,6};
+
+		System.out.println("Enter the value:");
+		int value=sc.nextInt();
+		
+		sc.close();
+		arrayValueDeletion(arr,value);
+		
+	}
+	static void arrayValueDeletion(int arr[],int value) {
+		boolean flag=false;
+		for(int x:arr) {
+			if(x==value) {
+				flag=true;
+				break;
+			}
+		}
+		if(flag) {
+			int []newArr=new int[arr.length-1];
+			System.out.println(Arrays.toString(arr));
+			for(int i=0;i<arr.length-1;i++) {
+				if(flag && arr[i]!=value) {
+					newArr[i]=arr[i];
+				}else {
+					flag=false;
+					newArr[i]=arr[i+1];
+				}
+			}
+			arr=newArr;
+			System.out.println(Arrays.toString(arr));
+		}else {
+			System.out.println("Value not found!");
+		}
+	}
+}
