807c641ee83a864dff3507b74b96bedf28ad44df
Shivam Mishra <126499350+shivammishra22@users.noreply.github.com>
2026-08-03 00:30:20 +0530

    Create ArrayCreation.java

---
+++ ArrayCreation.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayCreation {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Array Size:");
+		int n=sc.nextInt();
+		int []arr=new int[n];
+		for(int i=0;i<arr.length;i++) {
+			System.out.println("Enter Array Value:");
+			arr[i]=sc.nextInt();
+		}
+		sc.close();
+		
+		for(int x=0;x<arr.length;x++) {
+			System.out.print(arr[x]+" ");
+		}
+		System.out.println();
+		for(int x:arr) {
+			System.out.print(x+" ");
+		}
+		System.out.println();
+		System.out.println(Arrays.toString(arr));
+	}
+}
