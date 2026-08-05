cf02f7435d16e3d02682f8bfc89e0cdf50c0cb23
Shivam Mishra <126499350+shivammishra22@users.noreply.github.com>
2026-08-05 18:09:07 +0530

    Create 89DSA_Sorting_Insertion1.java

---
+++ 89DSA_Sorting_Insertion1.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class DSA_Sorting_Insertion1 {
+
+	public static void main(String[] args) {
+		int arr[]= {4,56,8,2,56,14,6,5,2,56,14,5};
+		// printing the array
+		System.out.println(Arrays.toString(arr));
+		
+		int len = arr.length; 
+		//Sorting in Increasing order
+        for (int i = 1; i < len; i++) {
+        	int key = arr[i]; 
+        	int j = i - 1;
+            while ( j >= 0 && key < arr[j]){
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        
+        //printing the sorted array
+        System.out.println(Arrays.toString(arr));
+	}
+}
